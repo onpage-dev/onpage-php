20bb79f24f913cec9f312ffce38924cc6ed4a245
Giacomo Rizzi <giacomo.rizzi@onpage.it>
2022-04-12 11:07:48 +0200

    #fix getOrder

--- src/Thing.php
+++ src/Thing.php
@@ -23,7 +23,7 @@ class Thing
 
     function getOrder(): float
     {
-        return $this->json->order;
+        return $this->id / 5_000_000_000 + $this->json->order;
     }
 
     function getFolderID():? int
